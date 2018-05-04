e84c93e01cf11fee708a9876c65a9ca28bbdcb01
Brent <brentdanis@gmail.com>
2018-05-04 14:07:10 -0700

    initial commit of gitObjects project

---
+++ assests/js/scripts.js
@@ -0,0 +1,10 @@
+var elem = document.querySelector('.fixed-action-btn');
+  var instance = M.FloatingActionButton.init(elem, {
+    direction: 'left'
+  });
+
+
+var elem = document.querySelector('.sidenav');
+	var instance = M.Sidenav.init(elem, options);
+
+
